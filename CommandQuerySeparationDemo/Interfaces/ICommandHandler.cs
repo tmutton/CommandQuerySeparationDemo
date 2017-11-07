a9077a23240f2701abfd6326ab62ad3c0e1f2c70
Thomas Mutton <tmutton1@gmail.com>
2017-11-07 21:49:43 +0000

    added project and command/query interfaces

---
+++ CommandQuerySeparationDemo/Interfaces/ICommandHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace CommandQuerySeparationDemo.Interfaces
+{
+    public interface ICommandHandler<TCommand>
+    {
+        void Handle(TCommand command);
+    }
+}
---
+++ CommandQuerySeparationDemo/Interfaces/IQueryHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace CommandQuerySeparationDemo.Interfaces
+{
+    public interface IQueryHandler<TResult>
+    {
+    }
+
+    public interface IQueryHandler<TResult, TQuery> where TQuery: IQueryHandler<TResult>
+    {
+        TResult Handle(TQuery query);
+    }
+}
